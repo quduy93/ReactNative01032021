60eb30d20ce2100ab21d7a85962582094ebe6172
Duy <duy_truong993@yahoo.com>
2021-03-05 21:07:50 +0700

    buoi 2 method array

--- 05032021.js
+++ 05032021.js
@@ -38,3 +38,48 @@ tinhTong([1,2,3,4,5], console.log)
 tinhTong([1,2,3,4,5], function(result) {
     console.log(result)
 })
+
+// 15 : MAP
+//1. Thay doi gia tri cua tung phan tu, khong thay doi kich thuoc mang
+//2. Khong gan su thay doi vao mang old, gan su thay doi vao mang new
+const arrNums = [1,2,3,4,5,6,7]
+const newArrNums = arrNums.map(function(number, index) {
+    return number*2
+})
+console.log(arrNums)
+console.log(newArrNums)
+
+nameOnly([
+    {
+        name:"Messi",
+        age: 20
+    },
+    {
+        name:"Neymar",
+        age: 22
+    },
+    {
+        name:"Ronaldo",
+        age: 24
+    }
+])
+
+function nameOnly(arr) {
+    const newArrMap = arr.map(function(person) {
+        return person.name
+    })
+    console.log(newArrMap)
+}
+
+// nang cap mang
+Array.prototype.myMap = function(fn) {
+    let newArr =  []
+    for (const value of this) {
+        newArr.push(fn(value))
+    }
+    return newArr
+}
+const newArrNums2 = arrNums.myMap(function(number) {
+    return number*2
+})
+console.log(newArrNums2)
